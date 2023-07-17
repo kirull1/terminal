676176508f2376f9221901afe5a0df3173d8a600
kirill22111333 <kivahnin@gmail.com>
2023-07-18 00:59:06 +0300

    refactor: moved functions from utils.js file to utils directory

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,4 +2,24 @@ function isFunction(functionToCheck) {
     return functionToCheck && {}.toString.call(functionToCheck) === "[object Function]";
 }
 
-export { isFunction };
+function replaceCharacter(string, index, replacement, replacementLength) {
+    if (replacementLength === undefined) {
+        replacementLength = replacement.length;
+    }
+
+    return (
+        string.slice(0, index) +
+      replacement +
+      string.slice(index + replacementLength)
+    );
+}
+
+function removeHTML(string) {
+    return string.replace(/<\/?[^>]+(>|$)/g, "");
+}
+
+function clearWindow(window, symbol) {
+    return window.map(element => element.replace(/./g, symbol));
+}
+
+export { clearWindow, replaceCharacter, removeHTML, isFunction };
